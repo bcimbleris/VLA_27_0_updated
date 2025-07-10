24b971e190086e838ff52bd46894003e17697f84
Bruno Cimbleris <bcimbleris@gmail.com>
2025-07-10 11:15:05 -0300

    Module 18
    Solves module 18

--- lab18-rcvr/rcvr.v
+++ lab18-rcvr/rcvr.v
@@ -1,6 +1,6 @@
 module rcvr
 #(
-  parameter [7:0] MATCH = 8'hA5
+  parameter [7:0] MATCH = 8'hA5 //10100101
  )
  (
   input  wire      clock   ,
@@ -27,19 +27,19 @@ module rcvr
         // INITIALIZE PHASE TO SHIFTING HEADER
         // INITIALIZE HEADER TO OPPOSITE OF LEFTMOST MATCH BIT
         // INITIALIZE CONTROL REGISTERS TO INACTIVE STATE (IGNORE DATA PATH)
-
-
-
-
-
-
+      phase <= SHIFT_HEAD;
+      head_reg <= 0;
+      ready <= 0;
+      overrun <= 0;
+      count <= 0;
       end
 
     else begin
 
         // IF PHASE IS SHIFT_BODY THEN CLEAR HEAD REGISTER, ELSE
         // IF PHASE IS SHIFT_HEAD THEN SHIFT INPUT DATA LEFT INTO HEAD REGISTER
-
+        if (phase == SHIFT_BODY) head_reg <= 0; else
+        if (phase == SHIFT_HEAD) head_reg <= {head_reg, data_in};
 
 
 
@@ -48,26 +48,27 @@ module rcvr
         // SET PHASE TO SHIFT_BODY, ELSE
         // IF COUNT IS 7 THEN SET PHASE BACK TO SHIFT_HEAD
 
-
+        if({head_reg, data_in} == MATCH) phase <= SHIFT_BODY; else
+        if(count == 7) phase <= SHIFT_HEAD;
 
 
 
         // IF PHASE IS SHIFT_BODY THEN INCREMENT COUNT
-
+        if (phase == SHIFT_BODY) count <= count + 1;
 
         // IF PHASE IS SHIFT_BODY THEN SHIFT INPUT DATA LEFT INTO BODY REGISTER
 
-
+        if (phase == SHIFT_BODY) body_reg <= {body_reg, data_in}; 
         // IF COUNT IS 7 THEN COPY CONCATENATION OF BODY REGISTER AND INPUT DATA
         // TO OUTPUT DATA
 
-
+        if (count == 7) data_out <= { body_reg, data_in } ;
         // IF COUNT IS 7 THEN SET READY ELSE IF READING THEN CLEAR READY
-
+        if (count == 7) ready <= 1; else if (reading) ready <= 0;
 
         // IF READING THEN CLEAR OVERRUN, ELSE
         // IF COUNT IS 7 AND STILL READY THEN SET OVERRUN
-
+        if (reading) overrun <= 0; else if (count==7 && ready) overrun <= 1;
 
       end
 
